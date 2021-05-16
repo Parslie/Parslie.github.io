7ffcc773f7b88415052665c50a69d7a7bdb6d5c9
Viktor Holta <viktor.holta@outlook.com>
2021-05-16 17:47:01 +0200

    NEW: add software entries via script

---
+++ scripts/data/software.js
@@ -0,0 +1,23 @@
+const entries = JSON.parse(`
+    [
+        {
+            "repoURL": "https://api.github.com/repos/Parslie/stardew-planney",
+            "title": "Stardew Planney",
+            "image": "",
+            "description": "A stardew valley farm planner with intuitive UI and controls.  // TODO: add to",
+            "tags": ["Utility", "CS", "Unity"]
+        },{
+            "repoURL": "https://api.github.com/repos/Parslie/outstanding-app",
+            "title": "Outstanding",
+            "image": "res/img/outstanding/login.jpg",
+            "description": "A social media app for android. // TODO: add to",
+            "tags": ["Social Media", "Java", "Kotlin", "Android Studio"]
+        },{
+            "repoURL": "https://api.github.com/repos/Parslie/robbe-robot",
+            "title": "Robbe Robot",
+            "image": "res/img/robbe robot/help.png",
+            "description": "A discord bot.  // TODO: add to",
+            "tags": ["Discord", "Bot", "Python"]
+        }
+    ]
+`)
---
+++ scripts/entryInjector.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let entryParent = document.getElementById("entries");
+
+    /* Generate each entry */
+    entries.forEach((entry, i) => {
+        let entrySection = document.createElement("section");
+        let entryHeader = document.createElement("header");
+        let entryContent = document.createElement("content");
+        let entryFooter = document.createElement("footer");
+
+        /* Header */
+        let title = document.createElement("h2");
+        title.className = "title";
+
+        fetch(entry.repoURL)
+        .then(resp => {
+            resp.json().then(json => {
+                let repoLink = document.createElement("a");
+                repoLink.href = json.html_url;
+                repoLink.innerHTML = entry.title;
+                title.appendChild(repoLink);
+
+                let date = json.updated_at;
+                let formattedDate = new Date(date).toLocaleString();
+
+                let updatedText = document.createElement("p");
+                updatedText.innerHTML = "Updated: " + formattedDate;
+                entryHeader.appendChild(updatedText);
+            });
+        })
+        .catch(e => {
+            title.innerHTML = entry.title;
+        });
+
+        entryHeader.appendChild(title);
+
+        /* Content */
+        if (entry.image != null && entry.image != "") {
+            let image = document.createElement("img");
+            image.src = entry.image;
+            entryContent.appendChild(image);
+        }
+
+        let description = document.createElement("p");
+        description.innerHTML = entry.description;
+        entryContent.appendChild(description);
+
+        /* Footer */
+        let tagList = document.createElement("ul");
+        tagList.className = "tags";
+        entryFooter.appendChild(tagList);
+
+        entry.tags.forEach((tag, i) => {
+            let tagItem = document.createElement("li");
+            tagItem.innerHTML = tag;
+            tagList.appendChild(tagItem);
+            tagList.appendChild(document.createTextNode("\u00A0")); /* Adds whitespace */
+        });
+
+        /* Append elements */
+        entryParent.appendChild(entrySection);
+        entrySection.appendChild(entryHeader);
+        entrySection.appendChild(entryContent);
+        entrySection.appendChild(entryFooter);
+    });
+});
