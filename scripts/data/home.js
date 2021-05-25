cad8eab7ebffba0d5794a617cc8fabad9a7d6a94
Viktor Holta <viktor.holta@outlook.com>
2021-05-25 19:11:23 +0200

    NEW: add/update entries on home page

--- scripts/data/home.js
+++ scripts/data/home.js
@@ -1,10 +1,31 @@
 const entries = JSON.parse(`
     [
         {
+            "title": "About Me",
+            "description": "Hello, world! My name is Viktor Holta—also known as Parslie—and I'm currently a software engineering student. These pages serve as a collection and portfolio of all my works and experiences. <b>(will add photo later)</b>",
+            "image": "res/img/ummm.jpg"
+        },{
             "title": "Programming Languages",
-            "description": "* <b>- language that I am not entirely comfortable with</b>",
+            "description": "These are programming languages I have previously used, meaning \
+                            I have used them for projects with more than a few \
+                            modules/scripts. <br /><br /> \
+                            * - <b>not technically programming languages</b>",
             "list": [
-                "C", "C++", "C#", "Java", "Kotlin", "Lua *", "Python"
+                "C", "C++", "C#", "CSS *", "HTML *", "Java", "JavaScript", "Kotlin", "Lua", "Python"
+            ]
+        },{
+            "title": "Programming IDEs",
+            "description": "These are programming IDEs I have previously used, meaning \
+                            I have used them for at least an hour in total.",
+            "list": [
+                "Android Studio", "Eclipse", "IntelliJ", "PyCharm", "Visual Studio"
+            ]
+        },{
+            "title": "Graphics/Art Editors",
+            "description": "These are graphics/art editors I have previously used, meaning \
+                            I fairly knowledgable of their capabilities and usages.",
+            "list": [
+                "Aseprite", "Photoshop"
             ]
         }
     ]
